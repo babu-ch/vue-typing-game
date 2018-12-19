527aaf93e3d12da964b080ad52f1a6e43edc7175
nena3 <nena3@example.com>
2018-12-19 18:36:38 +0900

    yes

---
+++ src/components/GameDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="gameDisplay">
+        display
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'GameDisplay',
+  }
+</script>
+
+<style scoped lang="scss">
+    .gameDisplay {
+        width: 800px;
+        height: 600px;
+        border: 1px solid #CCC;
+        margin: auto;
+    }
+</style>
