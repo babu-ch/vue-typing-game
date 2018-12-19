ab2726c74c379136737703fc252132f77c09e62b
nena3 <nena3@example.com>
2018-12-19 19:01:01 +0900

    途中

---
+++ src/components/parts/Strings.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="healthBar" :style="style">
+        This is test
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Strings',
+    data() {
+      return {
+        style: {
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+    div {
+        font-size: 60px;
+        margin-top: 122px;
+    }
+</style>
