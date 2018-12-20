f3ffd8b50b44a93612a18997dcc7b01aa0cdce57
nena3 <nena3@example.com>
2018-12-20 10:54:46 +0900

    inputstrings

---
+++ src/inputStrings.js
@@ -0,0 +1,17 @@
+
+export default [
+  {displayString:'邪銀三郎', inputString:'yokoshimaginzaburou'},
+  {displayString:'Release', inputString:'release'},
+  {displayString:'FAIL HARDER', inputString:'failharder'},
+  {displayString:'なかちサミュエル', inputString:'nakachisamuel'},
+  {displayString:'労働組合', inputString:'roudoukumiai'},
+  {displayString:'テストしたい', inputString:'testshitai'},
+  {displayString:'たびに出よう', inputString:'tabinideyou'},
+  {displayString:'ランチはちくわ', inputString:'ranchihachikuwa'},
+  {displayString:'ランチパック', inputString:'ranchipakku'},
+  {displayString:'マージボタンをおしただけ', inputString:'ma-jibotanwooshitadake'},
+  {displayString:'何もしてないのにこわれた', inputString:'nanimoshitenainonikowareta'},
+  {displayString:'まほうで動いている', inputString:'mahoudeugoiteiru'},
+  {displayString:'転職してラッパーになる', inputString:'tenshokushiterappa-ninaru'},
+]
+
