5e3d11045dbc99d282d343832f9aef2d87558c3b
nena3 <nena3@example.com>
2018-12-21 14:48:59 +0900

    result

--- src/components/GameEndDisplay.vue
+++ src/components/GameEndDisplay.vue
@@ -2,6 +2,9 @@
     <div class="gameDisplay">
         <h1>Game End</h1>
         <p>Success Stages: {{successStage}}</p>
+        <p>Total Type: {{totalTypeCount}}</p>
+        <p>Success Type: {{successTypeCount}}</p>
+        <p>Accuracy: {{accuracy}}%</p>
         <button @click="gameStart">Retry</button>
     </div>
 </template>
@@ -13,6 +16,21 @@
     computed: {
       successStage() {
         return this.$store.state.successStage
+      },
+      totalTypeCount() {
+        return this.successTypeCount + this.missTypeCount
+      },
+      successTypeCount() {
+        return this.$store.state.typeSuccessCount
+      },
+      missTypeCount() {
+        return this.$store.state.missCount
+      },
+      accuracy() {
+        if (this.totalTypeCount === 0) {
+            return 0
+        }
+        return Math.ceil((this.successTypeCount / this.totalTypeCount) * 100);
       }
     },
     methods: {
@@ -32,6 +50,6 @@
         position: relative;
     }
     h1 {
-        margin-top: 140px;
+        margin-top: 70px;
     }
 </style>
